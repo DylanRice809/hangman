997d447fd22588e581eb29038f2ab4f658c86cc3
Dylan Rice <dylanpatrickrice@gmail.com>
2023-05-11 17:17:35 +0100

    Allow user to make guesses
    Guesses will also be verified, and it will loop until failure or victory

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,17 +6,38 @@ class Hangman
   def initialize (all_words)
     @word = all_words.split(" ").select { |element| element.length >= 5 && element.length <= 12 }.sample
     @strikes = 8
-    @correct_guesses = []
+    @correct_guesses = Array.new(@word.length, "_")
     @incorrect_guesses = []
   end
 
-  def take_turn ()
-    # allow the player to make a guess
+  private
+  def find_all_index (letter, word, indices_array=[])
+    word.split("").each_with_index do |element, index|
+      if (element == letter)
+        indices_array << index
+      end
+    end
+    indices_array  
+  end
+  
+  def take_turn (word, guess_array, strikes, incorrect_guesses)
     guess = gets.chomp!
-    # verify if and where this letter takes place
-    
-    # if not, add a strike, and add to incorrect guesses
-    # if yes, position the guesses appropriately in the correct_guesses array, then check win
+    if (word.include?(guess))
+      find_all_index(guess, word).each { |index| guess_array[index] = guess }
+      p guess_array
+    else
+      strikes -= 1
+      incorrect_guesses << guess
+    end
+  end
+
+  public
+  def play_game ()
+    while (@strikes > 0 && @correct_guesses.include?("_")) do
+      take_turn(@word, @correct_guesses, @strikes, @incorrect_guesses)
+    end
+  end
 end
 
 new_game = Hangman.new(dictionary_contents)
+new_game.play_game
