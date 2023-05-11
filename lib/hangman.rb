4c27f00ceb74a09dcfc2d03b7601e37204cf0c43
Dylan Rice <dylanpatrickrice@gmail.com>
2023-05-11 18:27:43 +0100

    Add save functionality
    Accessed by typing save when guessing

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 dictionary = File.open("google-10000-english-no-swears.txt", "r")
 dictionary_contents = dictionary.read
 dictionary.rewind
@@ -23,6 +25,9 @@ class Hangman
   def take_turn (word, guess_array, incorrect_guesses)
     puts "Guess a letter: "
     guess = gets.chomp.downcase
+    if (guess == "save")
+      serialize()
+    end
     if (word.include?(guess))
       find_all_index(guess, word).each { |index| guess_array[index] = guess }
       puts "There are #{find_all_index(guess, word).length} #{guess}s."
@@ -33,13 +38,21 @@ class Hangman
     end
   end
 
+  def serialize
+    file = File.open('game.yml', 'w') do |f|
+      YAML.dump(self, f)
+    end
+    file.close
+    exit
+  end
+
   public
   def play_game ()
     loop do 
       puts "Strikes remaining: #{@strikes}"
       puts "Incorrect guesses: #{@incorrect_guesses}"
-      take_turn(@word, @correct_guesses, @incorrect_guesses)
       p @correct_guesses
+      take_turn(@word, @correct_guesses, @incorrect_guesses) 
       puts "--------------------"
       if (!(@correct_guesses.include?("_")))
         puts "Congratulations! You won!"
@@ -52,5 +65,17 @@ class Hangman
   end
 end
 
-new_game = Hangman.new(dictionary_contents)
-new_game.play_game
+def deserialize
+  File.open('game.yml', 'r') do |f|
+    loaded_game = YAML.load(f, permitted_classes: [Hangman])
+    loaded_game.play_game
+  end
+end
+
+puts "Would you like to reload your most recent save?"
+if (gets.chomp.downcase == "y")
+  deserialize()
+else
+  new_game = Hangman.new(dictionary_contents)
+  new_game.play_game
+end
