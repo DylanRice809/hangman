da374c133db5a174398772d899cadc3d333d89a3
Dylan Rice <dylanpatrickrice@gmail.com>
2023-05-11 16:39:29 +0100

    Restrict selection criteria
    Previouslt it would select a random word, but now it must be between 5 and 12 characters.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,5 @@
 dictionary = File.open("google-10000-english-no-swears.txt", "r")
 dictionary_contents = dictionary.read
 
-word = dictionary_contents.split(" ").sample
+word = dictionary_contents.split(" ").select { |element| element.length >= 5 && element.length <= 12 }.sample
 p word
